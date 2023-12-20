c4bc2af0a58050e3a9bd431153e4fbd21c7cc9e3
FI502527 <502527@student.fontys.nl>
2023-12-20 15:09:40 +0100

    Starting classes

--- Container Vervoer/Container Vervoer/Classes/Container.cs
+++ Container Vervoer/Container Vervoer/Classes/Container.cs
@@ -8,5 +8,30 @@ namespace Container_Vervoer.Classes
 {
     public class Container
     {
+        public string Naam { get; set; }
+        public int Gewicht { get; set; } //IN TON NIET KG
+        public Type Type { get; }
+
+        public Container(string naam, int gewicht, Type type)
+        {
+            Naam = naam;
+            GewichtMaken(gewicht); // SET ALS PROPERTY 
+            Type = type;
+        }
+        public void GewichtMaken(int gewicht) // PRIVATE
+        {
+            if (gewicht < 4)
+            {
+                Gewicht = 4; // VERVANG MET EXCEPTION
+            }
+            else if (gewicht > 30)
+            {
+                Gewicht = 30;
+            }
+            else
+            {
+                Gewicht = gewicht;
+            }
+        }
     }
 }
---
+++ Container Vervoer/Container Vervoer/Classes/Stapel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container_Vervoer.Classes
+{
+    public class Stapel
+    {
+        public List<Container> Containers = new List<Container>();
+
+        public bool IsErWaardevolleContainer()
+        {
+            bool status = false;
+            foreach (Container container in Containers)
+            {
+                if (container.Type == Type.Waardevol)
+                {
+                    status = true; // RETURN TRUE DIRECT
+                }
+            }
+            return status;
+        }
+        public int HoeveelContainers() // NAAMGEVING
+        {
+            return Containers.Count;
+        }
+        public int HoeZwaar() // NAAMGEVING
+        {
+            int gewicht = 0;
+            foreach (Container container in Containers)
+            {
+                gewicht += container.Gewicht;
+            }
+            return gewicht;
+        } // METHODE CHECK HOEVEEL GEWICHT BOVENOP CONTAINER
+        public bool IsErGenoegRuimte(Container container)
+        {
+            return HoeZwaar() + container.Gewicht < 120; // CONSTANT MAX GEWICHT
+        }
+        public bool VoegContainerToe(Container container)
+        {
+            if (IsErWaardevolleContainer() || !IsErGenoegRuimte(container))
+            {
+                return false;
+            }
+            Containers.Add(container);
+            return true;
+        }
+    }
+}
